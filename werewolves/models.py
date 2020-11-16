418a2a660031df6a8e7cb3c7c3a2120ee832ed29
Junwei Zhou <junwei23@bu.edu>
2020-11-15 23:02:35 -0500

    added updating game status, send system messages to server, updates need after adding role assigning

--- werewolves/models.py
+++ werewolves/models.py
@@ -14,6 +14,22 @@ class PlayerRole(Enum):
     def choices(cls):
         return tuple((i.name, i.value) for i in cls)
 
+# A game procedure has multiple steps, plus one unassigned:
+class GameStep(Enum):
+    WOLF = "WOLF"
+    SEER = "SEER"
+    GUARD = "GUARD"
+    ANNOUNCE = "ANNOUNCE"
+    SPEECH = "SPEECH"
+    VOTE = "VOTE"
+    END_GAME = "END_GAME"
+    END_VOTE = "END_VOTE"
+    NONE = "NOT ASSIGNED"
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
+
 # A Player can have three statuses.
 class PlayerStatus(Enum):
     ALIVE = "ALIVE"
@@ -40,17 +56,23 @@ class GameStatus(models.Model):
     # False means night, True means day.
     night           = models.BooleanField(default=False) 
     # Target player's id, null means the wolves haven't decided yet.
-    target          = models.IntegerField(null=True)
+    wolves_target          = models.IntegerField(null=True)
+    guard_target           = models.IntegerField(null=True)
+    seer_target            = models.IntegerField(null=True)
+    vote_target            = models.IntegerField(null=True)
+    # Indicate which speaker is the first speaker and which is the current speaker
+    first_speaker   = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_first_speaker", null=True)
+    current_speaker = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_current_speaker", null=True)
+    # Indicate the current game step
+    step = models.CharField(max_length=30, choices=GameStep.choices(), default=GameStep.NONE)
+    # Indicate if the game is over, False: wolves win, True: good people win, None: game is not over
+    winning         = models.BooleanField(null=True, default=None)
     # null means the player who got assigned the character is out.
     wolves          = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_wolf", null=True)
     seer            = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_seer", null=True)
     guard           = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_guard", null=True)
     villagers       = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_villager", null=True)
     # Booleans indicating whose turn this is at night (tentative).
-    wolves_turn     = models.BooleanField(default=False)
-    seer_turn       = models.BooleanField(default=False)
-    guard_turn      = models.BooleanField(default=False)
-    # Indicate which speaker is the first speaker and which is the current speaker
-    first_speaker   = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_first_speaker", null=True)
-    current_speaker = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="is_current_speaker", null=True)
-
+    #wolves_turn     = models.BooleanField(default=False)
+    #seer_turn       = models.BooleanField(default=False)
+    #guard_turn      = models.BooleanField(default=False)
